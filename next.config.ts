e3976529c5219f0ee43aaa492a8d6bbc6c56e7b5
ibelion <josh.13.c13@gmail.com>
2025-12-13 18:26:05 -0800

    Add leaguecontent/info rewrite and remove positions.ts dependency

--- next.config.ts
+++ next.config.ts
@@ -53,6 +53,10 @@ const nextConfig: NextConfig = {
         destination: `${CDN_BASE}/leaguecontent/images/:path*`,
       },
       {
+        source: "/leaguecontent/info/:path*",
+        destination: `${CDN_BASE}/leaguecontent/info/:path*`,
+      },
+      {
         source: "/pokemoncontent/pokemon/:path*",
         destination: `${CDN_BASE}/pokemoncontent/pokemon/:path*`,
       },
